2e54bbd87bfbaec9fe5bbcc809ea38d728c7cfcf
jpmunich <parkercoyle8@gmail.com>
2023-01-22 16:04:40 -0500

    Boilerplate + Heading

---
+++ odin-cv-card/src/Components/Cv.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import '../index.css';
+
+class Cv extends Component {
+    constructor(props) {
+        super(props) 
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return (
+            <div></div>
+        );
+    }
+}
+
+export default Cv;
---
+++ odin-cv-card/src/Components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../index.css';
+
+function Header() {
+    return (
+        <div className='header'>
+            <h1>CV Application</h1>
+        </div>
+    )
+}
+
+export default Header;
